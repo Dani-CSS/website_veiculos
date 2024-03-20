319084eb0aa2f0bb44e72a6a30bd29b6b6213623
Dani-CSS <danielecs26@gmail.com>
2024-03-20 08:12:51 -0300

    fix: resolve problema de formatação no patio

---
+++ dashfel/src/main/java/br/com/dcss/dashfel/dashfel/DashfelApplication.java
@@ -0,0 +1,13 @@
+package br.com.dcss.dashfel.dashfel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashfelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashfelApplication.class, args);
+	}
+
+}
---
+++ dashfel/src/test/java/br/com/dcss/dashfel/dashfel/DashfelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dcss.dashfel.dashfel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashfelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
