7c85e87dba021f23306df1254c191ce013b9e3d4
Dani-CSS <danielecs26@gmail.com>
2024-04-19 09:52:10 -0300

    style: melhorias

---
+++ dashfel/src/main/java/br/com/dcss/dashfel/dashfel/controller/ControllerVeiculos.java
@@ -0,0 +1,25 @@
+package br.com.dcss.dashfel.dashfel.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.dcss.dashfel.dashfel.model.Veiculo;
+import br.com.dcss.dashfel.dashfel.service.VeiculoService;
+
+@Controller
+@RequestMapping("/veiculos")
+public class ControllerVeiculos {
+
+    @Autowired
+    private VeiculoService veiculoService
+
+    @GetMapping
+    public String exibirFormulario(Modasdfl)
+
+
+
+
+
+}
---
+++ dashfel/src/main/resources/static/scripts.js
@@ -0,0 +1,9 @@
+function openCarousel() {
+    var popup = document.getElementById("carouselPopup");
+    popup.style.display = "block";
+}
+
+function closeCarousel() {
+    var popup = document.getElementById("carouselPopup");
+    popup.style.display = "none";
+}
